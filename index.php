08ea8d341ecd874daadb1fd20fa4939f48b469d1
Z1PL0X <jdzh07@gmail.com>
2023-03-20 15:43:40 -0500

    Fullstack

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php phpinfo();?>
+
+<h1>cualquier cosa</h1>
+
+<?php
+
+//Comentario de una linea, la siguiente linea es leida como codigo de programacion
+echo 'Linea de programacion';
+#Comentario de una linea, tiene la misma utilidad que "//".
+echo 'Linea de programacion';
+/* Comentario multilinea
+es capaz de abarcar muchas lineas,
+posee una etiqueta de apertura y otra de cierre */
+echo 'Linea de programacion';
+
+?>
